9a1dd2a60c945266f299bc4d9a1ca38030429093
Mikhail <rdbmw@mail.ru>
2022-05-09 23:48:22 +0300

    ДЗ 5.4. Оркестрация группой Docker контейнеров на примере Docker Compose

---
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gf8lssmei0qt0uvrbo"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gu60q161mtp7nb9arr"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8prss49phkq7fqi1j6"
+}
